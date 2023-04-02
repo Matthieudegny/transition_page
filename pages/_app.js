3c288346916aca6e64473f46f166e0d5011b77f8
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-04-02 15:29:40 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+import Navbar from "../Components/Navbar";
+import { AnimatePresence } from "framer-motion";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <div className="font-latoBold relative h-screen overflow-hidden py-24 px-12 lg:px-48">
+      <Navbar />
+      {/* initial false => at the mount of the component no animation */}
+      <AnimatePresence initial={false}>
+        <Component key={router.pathname} {...pageProps} />
+      </AnimatePresence>
+    </div>
+  );
 }
+
+export default MyApp;
